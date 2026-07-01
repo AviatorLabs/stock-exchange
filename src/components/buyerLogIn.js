37eb43a0e0be20337843dde5c9032ddd954b3860
Thomas Teklay <thomasteklay10@gmail.com>
2026-07-01 12:44:45 +0300

    Refactor buyer login form class and input IDs
    Updated id and class names to follow kebab-case naming convention.

--- src/components/buyerLogIn.js
+++ src/components/buyerLogIn.js
@@ -1,11 +1,11 @@
 export default function buyerLogIn(){
     return `
         <h2>Log-In as Buyer</h2>
-        <form class="logIN-form">
-            <label for="logIn-email">Account Email:</label>
-            <input type="email" id="logIn-email" name="buyerEmail" required class="buyer-form-input">
-            <label for="logIn-pass">Password:</label>
-            <input type="password" id="logIn-pass" name="buyerpass" required class="buyer-form-input">
+        <form class="login-form"> ${/* previous: class="logIN-form" */ ''}
+            <label for="login-email">Account Email:</label>
+            <input type="email" id="login-email" name="buyerEmail" required class="buyer-form-input"> ${/* previous: id="logIn-email" */ ''}
+            <label for="login-pass">Password:</label>
+            <input type="password" id="login-pass" name="buyerpass" required class="buyer-form-input"> ${/* previous: id="logIn-pass" */ ''}
             <button type="submit" class="buyer-form-btn">Log-In</button>
         </form>
     `
