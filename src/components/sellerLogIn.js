cc22d73d66b6fd910cfa3ba55642a10ca2583824
Thomas-Teklay-1 <thomasteklay10@gmail.com>
2026-07-08 17:11:39 +0300

    Updated the seller login file for name inconsistency

--- src/components/sellerLogIn.js
+++ src/components/sellerLogIn.js
@@ -1,11 +1,11 @@
 export default function sellerLogIn(){
     return `
-        <h2>Log-In as Seller</h2>
+        <h2>Log In as Seller</h2>
         <form class="login-form">
             <label for="login-email">Account Email:</label>
-            <input type="email" id="login-email" name="buyerEmail" required class="seller-form-input">
+            <input type="email" id="login-email" name="sellerEmail" required class="seller-form-input">
             <label for="login-pass">Password:</label>
-            <input type="password" id="login-pass" name="buyerPass" required class="buyer-form-input">
+            <input type="password" id="login-pass" name="sellerPass" required class="seller-form-input">
             <button  type="submit" class="seller-form-btn">Log-In</button>
         </form>
     `;
