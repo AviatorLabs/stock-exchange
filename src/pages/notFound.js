9741e3dd52e0897fbb6f6c29f7b2adf90318faac
Thomas Teklay <thomasteklay10@gmail.com>
2026-07-09 19:27:05 +0300

    Update the html indentation

--- src/pages/notFound.js
+++ src/pages/notFound.js
@@ -9,16 +9,11 @@ function render() {
         <main class="not-found-page">
 
             <h1>4<span class="o"0>0</span>4</h1>
-
             <h2>Page Not Found</h2>
-
             <p>
                 Sorry, the page you are looking for doesn't exist.
             </p>
-
-           <a data-link href="/" class="home-btn">
-    Return Home
-</a>
+           <a data-link href="/" class="home-btn">Return Home</a>
 
         </main>
     `;
