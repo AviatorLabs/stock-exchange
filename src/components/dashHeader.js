d6ec67d2f144b1d9786399118a0be1601b1210e7
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-01 10:25:40 +0300

    Fixed the dashboard header overflow

--- src/components/dashHeader.js
+++ src/components/dashHeader.js
@@ -3,9 +3,7 @@ export default function dashHeader(){
         <h2 class="dash-header-txt">Welcome</h2>
          <div class="header-right">
             <i class="fa-solid fa-user icon"></i>
-            <button type="button" id="logout-btn" class="logout-btn">
-    Logout
-</button>
+            <button type="button" id="logout-btn" class="logout-btn">Logout</button>
         </div>
     `;
 }
