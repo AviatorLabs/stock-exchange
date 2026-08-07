07e5edd180b154ca3f58821038e1bcc31e7d1130
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-07 12:28:17 +0300

    Added the stock initiation to the auth file

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,25 +1,39 @@
-import { state, setCurrentUser, setUserProfilePic } from "../state/state";
+import { state, setCurrentUser, setUserProfilePic, addStock } from "../state/state";
 
 export async function initApp() {
 
-    try{
+    try {
 
-        const response = await fetch("/api/me.php",{
-            credentials:"include"
+        const response = await fetch("/api/me.php", {
+            credentials: "include"
         });
 
         const result = await response.json();
 
-        if(result.success){
+        if (result.success) {
 
             setCurrentUser(result.user);
             setUserProfilePic(result.user.profilePicture);
+            initStocks()
             state.isLoggedIn = true;
         }
 
-    }catch(err){
+    } catch (err) {
 
         console.log(err);
     }
 
+}
+
+async function initStocks() {
+
+    const response = await fetch("/api/myStock.php", {
+        credentials: "include"
+    });
+
+    const result = await response.json();
+
+    if(result.success){
+        addStock(result.stocks);
+    }
 }
