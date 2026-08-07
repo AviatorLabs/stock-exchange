2b2b6453be5327329cee93815c9a7fa41ddb48ee
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-07 15:51:08 +0300

    created the backend that fetchs the data from database

---
+++ backend/api/marcket.php
@@ -0,0 +1,23 @@
+<?php
+    require "../config/database.php";
+
+    $stm = $pdo->prepare(
+        "SELECT * from stocks"
+    );
+
+    $stm->execute([]);
+
+    $stocks = $stm->fetchAll(PDO::FETCH_ASSOC);
+
+    if($stocks){
+        echo json_encode([
+            "success" => true,
+            "message" => "Fetch sucsessfull!"
+            "stocks" => $stocks
+        ]);
+    }else{
+        echo json_encode([
+            "success" => true,
+            "message" => "Unable To Fetch Stocks!"
+        ]);
+    }
