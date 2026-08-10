d9e6d1274f5b8be8440f60a0f220bb26792c9cd9
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-10 21:05:16 +0300

    Created the backend file

---
+++ backend/api/setStockHolder.php
@@ -0,0 +1,43 @@
+<?php
+
+require "../config/database.php";
+
+
+
+$data = json_decode(file_get_contents("php://input"), true);
+
+session_start();
+$buyer_id = $_SESSION["user_id"];
+$stock_id = $data["stockId"];
+$boughtByPrice = $data["boughtByPrice"];
+$quantityShare = $data["quantityShare"];
+
+try{
+    $stm = $pdo->prepare(
+        "INSERT INTO stockholders(stock_id, user_id, cost, share_quantity)
+        VALUES(:stock_id, :user_id, :cost, :share_quantity)"
+    );
+
+    $stm->execute([
+        ":stock_id" => $stock_id,
+        ":user_id" => $buyer_id,
+        ":cost" => $boughtByPrice,
+        ":share_quantity" => $quantityShare
+    ]);
+
+    echo json_encode([
+        "success" => true,
+        "message" => "Stock Holder set"
+        "stockHolder" => [
+            "stock_id" => $stock_id,
+            "cost" => $boughtByPrice,
+            "shareQuantity" => $quantityShare
+        ]
+    ])
+
+}catch (PDOException $e) {
+    echo json_encode([
+        "success" => false,
+        "message" => "Error: " . $e->getMessage()
+    ]);
+}
