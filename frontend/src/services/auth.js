44f1326864511ba8fb7af24e6a5154abdb81298b
Jon-INJC <chalachewyonathan8@gmail.com>
2026-08-10 11:18:32 +0300

    Corrected pasting error

--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -1,32 +1,51 @@
-import { state, addStock, addMarket } from '../state/state.js'
-import { getAvailableStock, getStockHolders, getBuyerHoldings } from './api.js'
-export async function setAvailableStock() {
+import { state, setCurrentUser, setUserProfilePic, addStock, addMarket } from "../state/state";
+import { getAvailableStock } from "../utils/api.js"
+export async function initApp() {
+
+    try {
+
+        const response = await fetch("/api/me.php", {
+            credentials: "include"
+        });
+
+        const result = await response.json();
+        await initAvailableStock()
+        if (result.success) {
+
+            await setCurrentUser(result.user);
+            await setUserProfilePic(result.user.profilePicture);
+            if(result.user.role === "seller"){
+                await initStocks()
+            }
+            state.isLoggedIn = true;
+        }
+
+    } catch (err) {
+
+        console.log(err);
+    }
+
+}
+
+async function initStocks() {
+
+    const response = await fetch("/api/myStock.php", {
+        credentials: "include"
+    });
+
+    const result = await response.json();
+
+    if (result.success) {
+        addStock(result.stocks);
+    }
+}
+
+async function initAvailableStock() {
     const result = await getAvailableStock();
+    const path = window.location.pathname;
+    console.log(path)
 
-    if (result.success) {
+    if (result.success && path === "/guest") {
         addMarket(result.stocks);
-    } else {
-        alert(result.message);
-    }
-}
-
-export async function setStock() {
-    const result = await getStockHolders();
-
-    if (result.success) {
-        addStock(result.stocks);
-    } else {
-        alert(result.message);
-    }
-
-}
-
-export async function setOwunedStock() {
-    const result = await getBuyerHoldings();
-
-    if (result.success) {
-        addStock(result.stocks);
-    } else {
-        alert(result.message);
     }
 }
